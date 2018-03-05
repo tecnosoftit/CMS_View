cfbd04b1ea77dec8354694322308321a74e1e693
TecnoSoftit <danielarevalo.1998@gmail.com>
2018-03-05 16:05:04 -0500

    m.

--- src/app/plans/plans.module.ts
+++ src/app/plans/plans.module.ts
@@ -7,10 +7,11 @@ import { NgbAccordionModule } from '@ng-bootstrap/ng-bootstrap';
 import { PlansComponent } from './plans.component';
 import { PlansRoutes } from './plans.routing';
 import { AppComponent } from '../app.component';
+import { CreateplanComponent } from './createplan/createplan.component';
 
 @NgModule({
   imports: [CommonModule, RouterModule.forChild(PlansRoutes), NgbAccordionModule],
-  declarations: [PlansComponent]
+  declarations: [PlansComponent, CreateplanComponent]
 })
 
 export class PlansModule {}
